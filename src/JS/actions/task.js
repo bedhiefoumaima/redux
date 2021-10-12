a63d5c40d6cfb9b9f44d54a5a643708880b68f1a
bedhiefoumaima <oumaimadhief@gmail.com>
2021-10-12 15:06:48 +0100

    first commit

---
+++ src/JS/actions/task.js
@@ -0,0 +1,16 @@
+import { ADD_TASK, DELETE_TASK, EDIT_TASK, STATUS_TASK } from "../constants/task"
+
+export const addTask=(newTask) => {
+    return {type : ADD_TASK, payload:newTask}
+};
+
+export const deleteTask=(id) => {
+    return {type : DELETE_TASK, payload:id}
+};
+export const editTask =(id,text)=>{
+    return{type:EDIT_TASK,payload:{id,text}}
+};
+export const statusTask=(id)=>{
+    return{type:STATUS_TASK,payload:{id}}
+}
+
---
+++ src/JS/constants/task.js
@@ -0,0 +1,7 @@
+export const ADD_TASK= "ADD_TASK"
+export const DELETE_TASK="DELETE_TASK"
+export const EDIT_TASK="DELETE_TASK"
+export const STATUS_TASK="DELETE_TASK"
+
+
+
